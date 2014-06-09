ba3b4d0f2ea68fa1f01ef518415083d9838d4778
thaismh <thaismha@gmail.com>
2014-06-09 18:27:28 -0300

    Removes .idea and target folders, create packages to model and controller

---
+++ src/test/java/com/springapp/mvc/controller/ObjectiveControllerTest.java
@@ -0,0 +1,12 @@
+package com.springapp.mvc.controller;
+
+import org.junit.Test;
+
+public class ObjectiveControllerTest {
+
+    @Test
+    public void prepareArgumentsToSend() throws Exception {
+
+
+    }
+}
---
+++ src/test/java/com/springapp/mvc/model/ObjectiveTest.java
@@ -0,0 +1,12 @@
+package com.springapp.mvc.model;
+
+import org.junit.Test;
+
+public class ObjectiveTest {
+
+    @Test
+    public void ReceivesDataFromRequest() throws Exception {
+
+
+    }
+}
